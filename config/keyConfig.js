db5607174ba594275584e7d98079a12e5c4e80ad
nrimal <rimal.5@osu.edu>
2018-10-27 10:50:06 -0400

    tweet api setup

---
+++ config/keyConfig.js
@@ -0,0 +1,12 @@
+
+const consumerKey = "DUZgThqmTjuduZyGikNU9NzLa";
+const consumerSecret = "7zRT1yKzgx6SAVNIG9dLZDrDhOc6KzJdvHaXi9F5fQsMAisxpc";
+const accessToken = "941173460230967297-rPWmgN2KJKW79wKQnppvtmUtnLFlzEw";
+const tokenSecret = "1kV6zrKV2FV3BoaJTJydda04cK2fBGThGCjE5GZHWNF9K";
+
+module.exports = {
+    consumerKey: consumerKey,
+    consumerSecret: consumerSecret,
+    accessToken: accessToken,
+    tokenSecret: tokenSecret
+}
--- index.js
+++ index.js
@@ -16,6 +16,47 @@ server.route({
     }
 });
 
+const Twitter = require('twitter');
+const config = require('./config/keyConfig');
+var client = new Twitter({
+    consumer_key: config.consumerKey,
+    consumer_secret: config.consumerSecret,
+    access_token_key: config.accessToken,
+    access_token_secret: config.tokenSecret
+});
+
+var requestScanning = [
+    { user: "ohackathon2018",  userID: 123, sinceID: 1 }
+]; //contains a users request for twitter profile to scan
+
+
+setInterval(() => {
+
+    requestScanning.forEach(req => {
+        var params = {
+            screen_name: req.user,
+            count: 50,
+            since_id: req.sinceID
+        };
+
+        let messagesToSend = []
+
+        client.get('statuses/user_timeline', params, function (error, tweets, response) {
+            if (error) {
+                console.log(error);
+                return;
+            }
+
+            tweets.forEach(tweet => {
+                console.log(tweet.text);
+            });
+            req.sinceID = tweets.length > 0 ? tweets[0].id : req.sinceID; //only update sinceID if tweets are found
+        });
+        // console.log("sinceID::" + params.since_id);
+    });
+}, 10000);
+
+
 const init = async () => {
 
     await server.start();
